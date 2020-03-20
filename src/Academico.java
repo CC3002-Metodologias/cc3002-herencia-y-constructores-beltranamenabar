b56f3bac3925923e7bf54e8cf9eaf41eab6bc425
Beltran <beltra.am@gmail.com>
2020-03-20 12:55:00 -0300

    Adding Constructors

--- src/Academico.java
+++ src/Academico.java
@@ -1,2 +1,7 @@
-public class Academico {
+public class Academico extends Persona{
+    private String paper;
+    public Academico(String nombre, String ocupacion, int edad, String paper){
+        super(nombre, ocupacion, edad);
+        this.paper = paper;
+    }
 }
--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,5 @@
-public class Estudiante {
+public class Estudiante extends Persona{
+    public Estudiante(String nombre, String ocupacion, int edad){
+        super(nombre, ocupacion, edad);
+    }
 }
