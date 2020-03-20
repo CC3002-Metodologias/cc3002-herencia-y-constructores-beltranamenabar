3b05c0e225fc59eb272b2aab7d689c35c20bd066
Beltran <beltra.am@gmail.com>
2020-03-20 13:34:26 -0300

    Fixing Constructor

--- src/Academico.java
+++ src/Academico.java
@@ -1,6 +1,7 @@
 public class Academico extends Persona{
     private String paper;
-    public Academico(String nombre, String ocupacion, int edad, String paper){
+
+    public Academico(String nombre, int edad, String paper){
         super(nombre, "Academico", edad);
         this.paper = paper;
     }
